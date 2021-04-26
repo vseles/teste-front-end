c4fca4bbcb17f4b837edb59ab21dae8d14b5eb69
vseles <vitor.hsseles@gmail.com>
2021-04-25 22:28:11 -0300

    add sliders

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,45 @@
-( function ( ) {
-  console.log('Hello World!');
-})();
+const onReady = function () {
+  setSliders();
+};
+
+const setSliders = function () {
+
+  // Banners Slider
+  $('#banners__slider').slick({
+    speed: 300,
+    dots: true,
+    arrows: true,
+    autoplay: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    appendArrows: $('#banners__controls'),
+    appendDots: $('#banners__controls')
+  });
+
+  // Brands Slider
+  $('#brands__slider').slick({
+    speed: 300,
+    dots: false,
+    arrows: true,
+    autoplay: true,
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    appendArrows: $('#brands__controls')
+  });
+
+  // Testimonial Slider
+  $('#testimonial__slider').slick({
+    speed: 300,
+    dots: false,
+    arrows: true,
+    autoplay: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    appendArrows: $('#testimonial__controls')
+  });
+};
+
+$(document).ready( onReady );
