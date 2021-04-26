aa77eb4c4c2e91ba0c240064183742189fef2c9c
vseles <vitor.hsseles@gmail.com>
2021-04-26 02:09:11 -0300

    add shelf component & get products JSON

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,5 +1,6 @@
 const onReady = function () {
   setSliders();
+  setShelf();
 };
 
 const setSliders = function () {
@@ -40,6 +41,56 @@ const setSliders = function () {
     slidesToScroll: 1,
     appendArrows: $('#testimonial__controls')
   });
+};
+
+const setShelf = function () {
+
+  const template = `
+    <section class="shelf">
+      <div class="shell">
+        <h2 class="shelf__title"> <span>Instrumentos</span> <strong>destaque</strong> </h2>
+        <p class="shelf__description">it is a long established fact that a reader will be distracted by the readable</p>
+        <ul class="shelf__list">
+          <li v-for="product in products">
+            <a href="javascript:void(0);" :title="product.productName">
+              <img :src="product.photo" :alt="product.productName" />
+              <h3 class="product__title">{{ product.productName }}</h3>
+              <p class="product__description">{{ product.descriptionShort }}</p>
+              <strong class="product__price">R$ {{ String( product.price.toFixed(2) ).replace('.', ',') }}</strong>
+            </a>
+          </li>
+        </ul>
+        <a class="shelf__button" href="javascript:void(0);">Ver mais</a>
+      </div>
+    </section>
+  `;
+
+  Vue.component('shelf-component', {
+    
+    template: template,
+
+    data: function () {
+
+      return {
+        products: []
+      };
+    },
+
+    created: function () {
+      
+      const _self = this;
+      const endpoint = `https://raw.githubusercontent.com/vseles/teste-front-end/master/produtos.json`;
+
+      $.getJSON( endpoint, function ( response ) {
+        if ( response && response.length > 0 )
+          _self.products = response;
+        else
+          alert('Não foi possível consultar os produtos.');
+      });
+    }
+  });
+
+  new Vue({ el: '#shelf-component' });
 };
 
 $(document).ready( onReady );
